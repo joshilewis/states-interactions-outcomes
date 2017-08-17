3a236fa4abf3f1c5a22537b85fe495b187aefd19
Joshua Lewis <joshilewis@gmail.com>
2017-08-17 11:02:26 +0200

    adds skeleton for CheckBalance specs

---
+++ ATM/CheckBalance.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ATM
+{
+    [TestFixture]
+    public class CheckBalance : Fixture
+    {
+        [Test]
+        public void AtmDisconnected()
+        {
+            Runner.RunScenario(
+                given => DisconnectedATM(),
+                when => ICheckBalance(),
+                then => Error_AtmDisconnected());
+
+        }
+
+        [Test]
+        public void IncorrectPin()
+        {
+            Runner.RunScenario(
+                given => ConnectedATM(),
+                when => ICheckBalanceWithIncorrectPin(),
+                then => Error_IncorrectPin());
+
+        }
+
+        [Test]
+        public void ZeroBalance()
+        {
+            Runner.RunScenario(
+                given => ConnectedATM(),
+                given => AccountBalanceIs(0),
+                when => ICheckBalance(),
+                then => ReportedBalanceIs(0));
+
+        }
+
+        [Test]
+        public void RegularBalance()
+        {
+            Runner.RunScenario(
+                given => ConnectedATM(),
+                given => AccountBalanceIs(100),
+                when => ICheckBalance(),
+                then => ReportedBalanceIs(100));
+
+        }
+
+        private void AccountBalanceIs(decimal actualBalance)
+        {
+        }
+
+        private void ReportedBalanceIs(decimal expectedBalance)
+        {
+
+        }
+
+        private void ConnectedATM()
+        {
+        }
+
+        private void ICheckBalanceWithIncorrectPin()
+        {
+        }
+
+        private void Error_IncorrectPin()
+        {
+        }
+
+        private void ICheckBalance()
+        {
+        }
+
+        private void Error_AtmDisconnected()
+        {
+        }
+
+        private void DisconnectedATM()
+        {
+
+        }
+    }
+}
---
+++ ATM/Fixture.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LightBDD;
+using LightBDD.Coordination;
+using LightBDD.Notification;
+using NUnit.Framework;
+
+namespace ATM
+{
+    [TestFixture]
+    public abstract class Fixture
+    {
+        protected BDDRunner Runner { get; private set; }
+
+        [OneTimeSetUp]
+        public void FixtureSetUp()
+        {
+            Runner = new BDDRunner(this.GetType(), CreateProgressNotifier());
+        }
+
+        [OneTimeTearDown]
+        public void FixtureTearDown()
+        {
+            FeatureCoordinator.Instance.AddFeature(Runner.Result);
+        }
+
+        protected virtual IProgressNotifier CreateProgressNotifier()
+        {
+            return new ConsoleProgressNotifier();
+        }
+
+    }
+}
